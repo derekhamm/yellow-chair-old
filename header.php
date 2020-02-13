de7f097961aab1d0a5b4b1cb25ed8d6e8b2990f5
professorhamm <derekh@tabor.edu>
2020-02-12 19:46:58 -0600

    color and font changes

--- header.php
+++ header.php
@@ -17,10 +17,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<link rel="stylesheet" href="https://use.typekit.net/zio5bjj.css">
+
 	<?php wp_head(); ?>
-	
-	<script src="https://use.typekit.net/vae2ufx.js"></script>
-    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+
 </head>
 
 <body <?php body_class(); ?>>
